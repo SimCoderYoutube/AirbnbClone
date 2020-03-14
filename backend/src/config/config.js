bd3fffed6220e19eb4f3172510fb8074ef3773f8
SimCoderYoutube <simplecodervideos@gmail.com>
2020-03-14 13:16:43 +0000

    [Lesson 7.1] Cleaned code

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,15 +1,15 @@
-databaseURL = 'https://airbnbclone-88e95.firebaseio.com'
+databaseURL = '*****'
 serviceAccount = {
-    "type": "service_account",
-    "project_id": "airbnbclone-88e95",
-    "private_key_id": "0beb787da2d2e3b768d0db5c0aba916177738c99",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCkz2bgZ9Ud4Shy\nxJ+87Xm77UgslgV3si98svZhvi63sanznqeIDQkamn1Hb6noUeHzVnTtT/dcmSAE\njV6mtkDUlGMtjwqItQiYhxMoObXAy3AKY3tKvyQHtzi1JEw2y8swEKLgRcVbeauC\nlUJcgQJcmMXrK4TO/GFPiVFz0O+O2cjmvRVE8S5p6k2EMbKahwMJe6kujX0B1Er0\nKZITkSmpm05cNEI8usV30aDWnchdJzPZAYDXQDS74EQeNq9EvczOgjAqQF9yS77j\nqk5oGp7U32YdyJpvmSq5aFESSrugqBiyXGh9fuGSWYlJPmYZv3XsyEGxEBUrNLGB\nyNgRl72RAgMBAAECggEAFTQ+oFpA5U5ohao+LQYBVrLhdCRnFq59BUEB3wh4z+x9\nQ66iulKHBykKO5cmbTv1DfmOeKaEWkxfKLFxlJG38exs0tnno+K6yjj9YicD9zFA\nEdjDw5Wnp+XPAiCQklo8+Er4/FGDDKZbm8+NUVkjJHu0DHI1wkBesBIoQcuHZBb0\nX3CkLVqnfYrFJlL1XwVB1DrwstdQJqeOrsv9XsrzrynWFNSBL+g1ONbktVWaPLm6\nZc7wl3ADmC+1Cl/gWBI0qpB8tUbIh3BEFtVG8UG5Oe2e0YKkWQ35csU5zUUOa/86\nTwphNQ0DEmWVqK/XUwBZeTV2nSnEGo5qgugPyXx7OwKBgQDeCF/0VvGbtC5ic72h\nkGmtmc75+HAIW9tpsn2iyRiGwb1OM4KaDK8c/l6Cf5YamNazlZdVlqzc65he6ZbE\nSLCndCLA5TYITnW6fmgcMOZOFL71ErRxHiE7k2N/ngXqfgXWe6JWLs6BmXRbwU8X\nnEendxnFaMPQ0kr7eJl7KMzErwKBgQC+BfrFKxmkkP/R8+fT32Hortumct9U+48X\na49HY+ygbs/KC5mvtGnDeWFhkA3PPsbyOQVeM7MA7ydtZec/86j45rPMygW8iMxM\n/wGf9cGCqNrBWshx7lhEdf6wJzi8zUTafKhxxRscXFvNWpcnGKS94Xh34Zrjn0Tp\n8ty/3WyxvwKBgQDaUc/QGQGfQY9ufTIUQES5T4jGIHRAs9nOV5TT0PqN3si+sUWV\nJi21y6SwmrXE104d7VSN8wdsfzgm83as8HDLZxWZKIh1qHh4dTz3o1mBo0KVBdxp\ncd/pjaaDYKWeimZBHPZl2cZyhqp0YmLb625Fi4XKp5YLqgvSsyXvhRG7vwKBgQC6\nYDUIo/Bft5QiQd70sRlf1fk79VlVCfLb3rRGKhzRcj49hkkzy3kcr3Wy8k6JIaKR\nuxmnvn9ogCikpRk+YWcu0WAykUc1FtuC/W6DjNBCrgj5Mmh9Tm+mnjCyZ5W/oHlS\n7odsJw4jL/ceoCa2wvX6AyZcNpvre7Bk4b5gNPZCEwKBgFA7t2EQX3/hWrvZdsXn\nocWQX5ZIMOrSfNJxonz2Bmzdk3uQ5Gu6mOUFtOxmWS8hRSDhS7m2ivAwNm4uP2jD\n33kDAw/wEN65xsOdm1fHCbAYp21uRgE6phW7wV6Bzsc69pZF1JjvRJ7kKXQZt2dX\nBly8QVtX5tAYCvbVHfbbu56a\n-----END PRIVATE KEY-----\n",
-    "client_email": "firebase-adminsdk-6rx19@airbnbclone-88e95.iam.gserviceaccount.com",
-    "client_id": "111903884824796922065",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-6rx19%40airbnbclone-88e95.iam.gserviceaccount.com"
+    "type": "*****",
+    "project_id": "*****",
+    "private_key_id": "*****",
+    "private_key": "*****",
+    "client_email": "*****",
+    "client_id": "*****",
+    "auth_uri": "*****",
+    "token_uri": "*****",
+    "auth_provider_x509_cert_url": "*****",
+    "client_x509_cert_url": "*****"
   }
   
 
